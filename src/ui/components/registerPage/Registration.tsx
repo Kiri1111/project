59e522753caf5da8f0fdd1f08436089641aafcdc
MalenkovDN <m18dm.dm@gmail.com>
2023-02-05 23:04:52 +0300

    added registration form

--- src/ui/components/registerPage/Registration.tsx
+++ src/ui/components/registerPage/Registration.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import style from './Registration.module.scss'
+import {useFormik} from 'formik';
+import Input from "../../common/components/commonInput/Input";
+import Button from "../../common/components/commonButton/Button";
+
 
 export const Registration = () => {
     return (
         <div className={style.registerBlock}>
             <h1>Register page</h1>
+            <form>
+                <Input />
+                <Input />
+                <Input />
+                <Button title={'Sign Up'}/>
+            </form>
         </div>
     );
 };
