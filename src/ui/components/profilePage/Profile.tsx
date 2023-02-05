fd3b2374d7f3d73438b03ce7c47ec1c751cd081e
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2023-02-05 11:32:15 +0300

    feat(profile):add navigate

--- src/ui/components/profilePage/Profile.tsx
+++ src/ui/components/profilePage/Profile.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import style from './Profile.module.scss'
 import {useAppSelector} from "../../../hooks/redux";
+import {Navigate} from "react-router-dom";
 
 export const Profile = () => {
     const user = useAppSelector(state => state.profile)
+    const isLoggedIn = useAppSelector(state => state.auth)
+    if (!isLoggedIn) {
+        return <Navigate to={'/login'}/>
+    }
     return (
         <div className={style.profileBlock}>
             <h1>My profile</h1>
