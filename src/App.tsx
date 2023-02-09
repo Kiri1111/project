9f5ce84fccb7ede24bc4c0668b6456785585d8ea
MalenkovDN <m18dm.dm@gmail.com>
2023-02-09 15:53:44 +0300

    feat(registration): added catching errors on registartion

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import style from './App.module.scss'
-import {Demo} from "./ui/components/demoPage/Demo";
-import {Header} from "./ui/components/header/header";
-import {Navigate, Route, Routes} from "react-router-dom";
-import {Login} from "./ui/components/loginPage/Login";
-import {NewPassword} from "./ui/components/newPasswordPage/NewPassword";
-import {RecoveryPassword} from "./ui/components/passwordRecoveryPage/RecoveryPassword";
-import {Profile} from "./ui/components/profilePage/Profile";
-import {Registration} from "./ui/components/registerPage/Registration";
-import {ErrorNotFound} from "./ui/components/errorNotFoundPage/ErrorNotFound";
-import {useAppDispatch, useAppSelector} from "./hooks/redux";
-import {initializeAppTC} from "./bll/reducers/auth";
-import {Preloader} from "./ui/common/components/preloader/Preloader";
-import {cardsApi} from "./dal/api/CardsApi";
+import { Demo } from "./ui/components/demoPage/Demo";
+import { Header } from "./ui/components/header/header";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Login } from "./ui/components/loginPage/Login";
+import { NewPassword } from "./ui/components/newPasswordPage/NewPassword";
+import { RecoveryPassword } from "./ui/components/passwordRecoveryPage/RecoveryPassword";
+import { Profile } from "./ui/components/profilePage/Profile";
+import { Registration } from "./ui/components/registerPage/Registration";
+import { ErrorNotFound } from "./ui/components/errorNotFoundPage/ErrorNotFound";
+import { useAppDispatch, useAppSelector } from "./hooks/redux";
+import { initializeAppTC } from "./bll/reducers/auth";
+import { Preloader } from "./ui/common/components/preloader/Preloader";
+import { cardsApi } from "./dal/api/CardsApi";
 
 export const App = () => {
     const dispatch = useAppDispatch()
@@ -24,24 +24,23 @@ export const App = () => {
     }, [])
 
     if (isInitialized) {
-        return <div
-            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
-            <Preloader width={'300px'}/>
+        return <div style={{ position: 'fixed', top: '30%', textAlign: 'center', width: '100%' }}>
+            <Preloader width={'300px'} />
         </div>
     }
 
     return (
         <div className={style.app}>
-            <Header/>
+            <Header />
             <Routes>
-                <Route path={'/project'} element={<Demo/>}/>
-                <Route path={'/*'} element={<Navigate to={'/404'}/>}/>
-                <Route path={'/404'} element={<ErrorNotFound/>}/>
-                <Route path={'/login'} element={<Login/>}/>
-                <Route path={'/newPassword'} element={<NewPassword/>}/>
-                <Route path={'/recoveryPassword'} element={<RecoveryPassword/>}/>
-                <Route path={'/profile'} element={<Profile/>}/>
-                <Route path={'/registration'} element={<Registration/>}/>
+                <Route path={'/project'} element={<Demo />} />
+                <Route path={'/*'} element={<Navigate to={'/404'} />} />
+                <Route path={'/404'} element={<ErrorNotFound />} />
+                <Route path={'/login'} element={<Login />} />
+                <Route path={'/newPassword'} element={<NewPassword />} />
+                <Route path={'/recoveryPassword'} element={<RecoveryPassword />} />
+                <Route path={'/profile'} element={<Profile />} />
+                <Route path={'/registration'} element={<Registration />} />
             </Routes>
         </div>
     );
--- src/ui/components/registerPage/Registration.tsx
+++ src/ui/components/registerPage/Registration.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import style from './Registration.module.scss'
-import {useFormik} from 'formik';
+import { useFormik } from 'formik';
 import Button from '../../common/components/commonButton/Button';
-import {RegistrationInputLabel} from './RegistrationInputLabel';
+import { RegistrationInputLabel } from './RegistrationInputLabel';
 import * as yup from 'yup';
-import {registerTC} from '../../../bll/reducers/registration';
-import {useAppDispatch, useAppSelector} from '../../../hooks/redux';
-import {Navigate} from 'react-router-dom';
+import { registerTC } from '../../../bll/reducers/registration';
+import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
+import { Navigate } from 'react-router-dom';
+import { Preloader } from '../../common/components/preloader/Preloader';
 
 
 type FormValuesType = {
@@ -19,6 +20,8 @@ export const Registration: React.FC = () => {
 
     const dispatch = useAppDispatch();
     const isLoginIn = useAppSelector(state => state.registration.isLoginIn);
+    const loading = useAppSelector(state => state.registration.loading);
+    const error = useAppSelector(state => state.registration.error);
 
     const chema = yup.object().shape({
         email: yup.string().matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Invalid email address').typeError('Invalid email address').required('Required'),
@@ -27,11 +30,13 @@ export const Registration: React.FC = () => {
     });
 
     const compareStrAndLength = (a: string, b: string) => {
-        if(a.length && b.length && a.localeCompare(b) === 0) {
+        if (a.length && b.length && a.localeCompare(b) === 0) {
             return false
         }
         return true
     }
+
+
 
     const formik = useFormik({
         initialValues: {
@@ -42,25 +47,31 @@ export const Registration: React.FC = () => {
         validationSchema: chema,
         onSubmit: (values: FormValuesType) => {
             dispatch(registerTC(values.email, values.password));
+            formik.resetForm();
         }
     });
 
-    if(isLoginIn) {
-        return <Navigate to={'/project'}/>;
+    if (loading) {
+        return <div style={{ position: 'fixed', top: '30%', textAlign: 'center', width: '100%' }}><Preloader width={'300px'} /></div>
+    }
+
+    if (isLoginIn) {
+        return <Navigate to={'/project'} />;
     }
 
     return (
 
         <div className={style.registerBlock}>
             <h1>Register page</h1>
+            <div><span style={{color: 'red', fontSize: '24px'}}>{error}</span></div>
             <form>
                 <RegistrationInputLabel text='Email' {...formik.getFieldProps('email')} />
                 {formik.touched.email && formik.errors.email && <div style={{ color: "red" }}>{formik.errors.email}</div>}
-                <RegistrationInputLabel text='Password' {...formik.getFieldProps('password')} type={'password'}/>
+                <RegistrationInputLabel text='Password' {...formik.getFieldProps('password')} type={'password'} />
                 {formik.touched.password && formik.errors.password && <div style={{ color: "red" }}>{formik.errors.password}</div>}
-                <RegistrationInputLabel text='Confirm password' {...formik.getFieldProps('confirmPassword')} type={'password'}/>
+                <RegistrationInputLabel text='Confirm password' {...formik.getFieldProps('confirmPassword')} type={'password'} />
                 {formik.touched.confirmPassword && formik.errors.confirmPassword && <div style={{ color: "red" }}>{formik.errors.confirmPassword}</div>}
-                <Button title={'Sign Up'} onClickCallBack={formik.handleSubmit} type={'button'} disabled={compareStrAndLength(formik.values.password, formik.values.confirmPassword)}/>
+                <Button title={'Sign Up'} onClickCallBack={formik.handleSubmit} type={'button'} disabled={compareStrAndLength(formik.values.password, formik.values.confirmPassword)} />
             </form>
         </div>
     );
